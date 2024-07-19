4dfb6011799d74bad57af8af29a710fa1d057c66
Johnathan <jdhwang98@gmail.com>
2024-07-19 06:25:08 -0700

    added model topic and migrated database

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    # CharField is used to store small amount of text
+    text = models.CharField(max_length=200)
+    # DateTimeField() records date and time
+    # auto_now_add=True tells Django to automatically add current date and time
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    """ This method returns the value assigned to text attribute """
+    def __str__(self): 
+        return self.text
+    
+
+
